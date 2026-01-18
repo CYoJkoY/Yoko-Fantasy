61fef55ef0e9a5df2443d4fb0c7ffeb22eed8c12
CYoJkoY <Cjy3430525946@outlook.com>
2026-01-18 21:17:57 +0800

    Refactor holy/soul UI logic and update translations
    Renamed and refactored holy and soul UI display/process functions for clarity and added mouse hover info popups. Updated stat initialization in player_run_data.gd. Translation files and CSV were updated to use zh_Hans_CN, with new info strings added and obsolete translation files removed. Minor fix to looting goblin behaviour description key.

--- extensions/main.gd
+++ extensions/main.gd
@@ -9,15 +9,15 @@ const UI_SOUL_SCENE = preload("res://mods-unpacked/Yoko-Fantasy/content/scenes/u
 # =========================== Extention =========================== #
 func _on_EntitySpawner_players_spawned(players: Array) -> void:
     ._on_EntitySpawner_players_spawned(players)
-    _yztato_holy_display()
-    _yztato_soul_display()
+    _fantasy_holy_display()
+    _fantasy_soul_display()
 
 func _process(_delta: float) -> void:
-    _yztato_holy_process()
-    _yztato_soul_process()
+    _fantasy_holy_process()
+    _fantasy_soul_process()
 
 # =========================== Custom =========================== #
-func _yztato_holy_display() -> void:
+func _fantasy_holy_display() -> void:
     for i in range(_players.size()):
         if _players[i] in UIHolyScenes:
             continue
@@ -31,14 +31,20 @@ func _yztato_holy_display() -> void:
         if !is_instance_valid(player_ui) or !is_instance_valid(player_ui.hud_container):
             continue
 
-        var gold_index = player_ui.hud_container.get_children().find(player_ui.gold)
+        var after_gold_index = player_ui.hud_container.get_children().find(player_ui.gold) + 1
         player_ui.hud_container.add_child(UIHolyInstance)
-        player_ui.hud_container.move_child(UIHolyInstance, gold_index)
+        player_ui.hud_container.move_child(UIHolyInstance, after_gold_index)
         
         UIHolyInstance.update_value(RunData.get_player_effect("fantasy_stat_holy", i))
+
+        if not UIHolyInstance.is_connected("mouse_entered", self, "fa_on_UIHoly_mouse_entered"):
+            UIHolyInstance.connect("mouse_entered", self, "fa_on_UIHoly_mouse_entered", [Utils.get_stat("fantasy_stat_holy", i)])
+        if not UIHolyInstance.is_connected("mouse_exited", self, "fa_on_UIHoly_mouse_exited"):
+            UIHolyInstance.connect("mouse_exited", self, "fa_on_UIHoly_mouse_exited")
+        
         UIHolyScenes[_players[i]] = UIHolyInstance
 
-func _yztato_soul_display() -> void:
+func _fantasy_soul_display() -> void:
     for i in range(_players.size()):
         if _players[i] in UISoulScenes:
             continue
@@ -52,21 +58,46 @@ func _yztato_soul_display() -> void:
         if !is_instance_valid(player_ui) or !is_instance_valid(player_ui.hud_container):
             continue
 
-        var gold_index = player_ui.hud_container.get_children().find(player_ui.gold)
+        var after_gold_index = player_ui.hud_container.get_children().find(player_ui.gold) + 1
         player_ui.hud_container.add_child(UISoulInstance)
-        player_ui.hud_container.move_child(UISoulInstance, gold_index)
+        player_ui.hud_container.move_child(UISoulInstance, after_gold_index)
         
-        RunData.get_player_effect("fantasy_stat_soul", i)
+        UISoulInstance.update_value(RunData.get_player_effect("fantasy_stat_soul", i))
+        
+        if not UISoulInstance.is_connected("mouse_entered", self, "fa_on_UISoul_mouse_entered"):
+            UISoulInstance.connect("mouse_entered", self, "fa_on_UISoul_mouse_entered", [Utils.get_stat("fantasy_stat_soul", i)])
+        if not UISoulInstance.is_connected("mouse_exited", self, "fa_on_UISoul_mouse_exited"):
+            UISoulInstance.connect("mouse_exited", self, "fa_on_UISoul_mouse_exited")
+        
         UISoulScenes[_players[i]] = UISoulInstance
 
-func _yztato_holy_process() -> void:
+func _fantasy_holy_process() -> void:
     for i in range(_players.size()):
         if _players[i] in UIHolyScenes and \
         is_instance_valid(UIHolyScenes[_players[i]]):
-            UIHolyScenes[_players[i]].update_value(RunData.get_player_effect("fantasy_stat_holy", i))
+            UIHolyScenes[_players[i]].update_value(Utils.get_stat("fantasy_stat_holy", i))
 
-func _yztato_soul_process() -> void:
+func _fantasy_soul_process() -> void:
     for i in range(_players.size()):
         if _players[i] in UISoulScenes and \
         is_instance_valid(UISoulScenes[_players[i]]):
-            UISoulScenes[_players[i]].update_value(RunData.get_player_effect("fantasy_stat_soul", i))
+            UISoulScenes[_players[i]].update_value(Utils.get_stat("fantasy_stat_soul", i))
+
+# =========================== Method =========================== #
+func fa_on_UIHoly_mouse_entered(stat_holy: int) -> void :
+    if _cleaning_up:
+        var damage_bouns = str(stat_holy) + "%"
+        var chance_drop_soul = str(int(stat_holy / (stat_holy + 50.0) * 100)) + "%"
+        var enemy_health_reduction = str(int(stat_holy / (stat_holy + 100.0) * 100)) + "%"
+        _info_popup.display(_ui_bonus_gold, Text.text("FANTASY_INFO_HOLY", [damage_bouns, chance_drop_soul, enemy_health_reduction]))
+
+func fa_on_UIHoly_mouse_exited() -> void :
+    _info_popup.hide()
+
+func fa_on_UISoul_mouse_entered(stat_soul: int) -> void :
+    if _cleaning_up:
+        var duration = str(stat_soul * 2)
+        _info_popup.display(_ui_bonus_gold, Text.text("FANTASY_INFO_SOUL", [duration]))
+
+func fa_on_UISoul_mouse_exited() -> void :
+    _info_popup.hide()
--- extensions/player_run_data.gd
+++ extensions/player_run_data.gd
@@ -1,18 +1,36 @@
 extends "res://singletons/player_run_data.gd"
 
+static func init_stats(all_null_values: bool = false)->Dictionary:
+
+    if (not Utils == null) :
+        var vanilla_stats = .init_stats(all_null_values)
+
+        var new_stats: = {
+            
+            "fantasy_stat_holy": 0,
+            "fantasy_stat_soul": 0,                                                     # Debug : Only For Assert
+            
+        }
+
+        new_stats.merge(vanilla_stats)
+
+        return new_stats;
+    else:
+        return {}
+
 static func init_effects()->Dictionary:
 
     if (not Utils == null) :
+        var mod_stats = init_stats()
         var vanilla_effects = .init_effects()
 
         var new_effects: = {
             
             "fantasy_original_speed": 0.0,
-            "fantasy_stat_holy": 110,
-            "fantasy_stat_soul": 100,
 
         }
-
+        
+        new_effects.merge(mod_stats)
         new_effects.merge(vanilla_effects)
 
         return new_effects;
