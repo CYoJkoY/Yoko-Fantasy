075801e14ac5917cb6acc4fb94ea6778f6570977
CYoJkoY <Cjy3430525946@outlook.com>
2026-05-24 06:56:04 +0800

    0520

--- dlc_1_data.gd
+++ dlc_1_data.gd
@@ -30,6 +30,7 @@ func _fantasy_curse_item(item_data: ItemParentData, _player_index: int, turn_ran
                 # Second process
                 new_effect.value = 0 if new_effect.value == 1 else new_effect.value
                 new_effect.chance = Utils.ncl_curse_effect_value(new_effect.chance, effect_modifier, {"step": 1})
+                new_effect.curse_num = Utils.ncl_curse_effect_value(new_effect.curse_num, effect_modifier, {"step": 1})
 
             ["fantasy_melee_pet", _, _], \
             ["fantasy_ranged_pet", _, _]:
